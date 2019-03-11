ade0a76c01e95cf246e9d1ef05fb598c25ae11d6
Frank <fr.heggeness@gmail.com>
2019-03-11 10:20:59 -1000

    sumallpositive

--- exercises.js
+++ exercises.js
@@ -239,7 +239,15 @@ function multiplyAll(arr){
  * @param {Array}
  * @return {Number}
  */
-var sumAllPositive;
+function sumAllPositive(arr){
+  var sum = 0;
+  for(i=0;i<arr.length;i++){
+    if(arr[i]>0){
+      sum += arr[i]
+    }
+  }
+  return sum;
+}
 
 /* #stringCountBelowThree
  *
